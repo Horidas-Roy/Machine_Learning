50fa24211adf97284caa5d6cb918def8f52b33cf
Horidas Roy <royhoridas64@gmail.com>
2025-10-16 22:01:05 +0600

    practice day - 8.5

---
+++ python Basic/week - 2 practice/Practice Day - 8.5/smartVehicleClassifier.py
@@ -0,0 +1,66 @@
+class Vehicle:
+    vehicles = []
+    def __init__(self,vehicle_type,name,speed):
+        self.__vehicle_type = vehicle_type
+        self.__name = name
+        self.__speed = speed
+        self.vehicles.append(self)
+
+
+    def classify(self,speed):
+        if speed > 100:
+            return "High-speed Vehicle"
+        else:
+            return "Normal Vehicle" 
+    @property
+    def get_name(self):
+        return self.__name
+    @property
+    def get_vehicle_type(self):
+        return self.__vehicle_type
+    @property
+    def get_speed(self):
+        return self.__speed
+    
+    
+class Car(Vehicle):
+    def __init__(self, vehicle_type, name, speed):
+        super().__init__(vehicle_type, name, speed)
+        print(f"Car Added: {self.get_name}")
+    
+    def show_info(self):
+        print("\n---Vehicle Details---")
+        for vehicle in self.vehicles:
+            print(f"Type: {vehicle.get_vehicle_type}, Name: {vehicle.get_name}, Speed: {vehicle.get_speed}",end=" ")
+            if vehicle.get_speed > 100:
+                print("Predicted: High-speed Vehicle")
+            else:
+                print("Predicted: Normal Vehicle")
+
+class Bike(Vehicle):
+    def __init__(self, vehicle_type, name, speed):
+        super().__init__(vehicle_type, name, speed)
+        print(f"Bike Added: {self.get_name}")
+    
+    def show_info(self):
+        print("---Vehicle Details---")
+        for vehicle in self.vehicles:
+            print(f"Type: {vehicle.get_vehicle_type}, Name: {vehicle.get_name}, Speed: {vehicle.get_speed}",end=" ")
+            if vehicle.get_speed > 100:
+                print("Predicted: High-speed Vehicle")
+            else:
+                print("Predicted: Normal Vehicle")
+
+
+
+
+n = int(input())
+for i in range(0,n):
+    vehicle_type,name,speed = map(str,input().split())
+    speed = int(speed)
+    if vehicle_type == "Car":
+        car = Car(vehicle_type,name,speed)
+    else:
+        bike = Bike(vehicle_type,name,speed)
+
+car.show_info()
